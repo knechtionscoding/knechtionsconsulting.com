749e7f0ef4d0d99337b1fbc41321c2e70771dd3d
Hans Knecht <hans@anomalo.com>
2024-04-16 21:55:48 +0200

    feat: add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,12 +17,22 @@ module.exports = {
         blogSettings: {
           path: "/blog", // Defines the slug for the blog listing page
           usePathPrefixForArticles: false, // Default true (i.e. path will be /blog/first-article)
+        }
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-Y1VJSLGR2S", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
         },
-        // googleAnalytics: {
-        //     trackingId: "UA-XXXXXX-X",
-        //     anonymize: true, // Default true
-        //     environments: ["production", "development"] // Default ["production"]
-        // }
       },
     },
   ],
